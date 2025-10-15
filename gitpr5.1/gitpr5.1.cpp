91c5b013bdf497763a88f9be26a2d48bb71d4529
Mimi4ok <fonarik021107@gmail.com>
2025-10-15 03:11:56 +0300

    first commit

---
+++ gitpr5.1/gitpr5.1.cpp
@@ -0,0 +1,37 @@
+// gitpr5.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double k(double x, double y);
+
+int main()
+{
+    double p, q;
+    cout << "p = "; cin >> p;
+    cout << "q = "; cin >> q;
+
+
+	double c = (k(1 + p * q, q * q) + pow(k(p, p * p), 2)) / (1 + k(p * q + q * q, p));
+
+    cout << "result = " << c << endl;
+    return 0;
+}
+
+double k(double x, double y)
+{
+    return x / (1 + pow(sin(y), 2)) + y / (1 + pow(x, 2));
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
